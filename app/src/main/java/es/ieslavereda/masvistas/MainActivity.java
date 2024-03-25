541e4ae71a847a70e2507c1425296672dcd9c423
JuanCarlosLaVereda <juasanper2@alu.edu.gva.es>
2024-03-25 18:34:33 +0100

    v.1

--- app/src/main/java/es/ieslavereda/masvistas/MainActivity.java
+++ app/src/main/java/es/ieslavereda/masvistas/MainActivity.java
@@ -4,17 +4,35 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText entrada;
+    private TextView salida;
+
+    private Button boton0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        entrada = findViewById(R.id.entrada);
+        salida = findViewById(R.id.salida);
+        boton0 = findViewById(R.id.boton0);
+
+        boton0.setOnClickListener(view -> entrada.setText(entrada.getText()+view.getTag().toString()));
     }
 
     public void sePulsa(View view){
-        Toast.makeText(this,"Pulsado", Toast.LENGTH_SHORT).show();
+        //Toast.makeText(this,"Pulsado", Toast.LENGTH_SHORT).show();
+        salida.setText(String.valueOf(Double.parseDouble(entrada.getText().toString())*2));
     }
+
+/*    public void sePulsa0(View view){
+        entrada.setText(entrada.getText()+view.getTag().toString());
+    }*/
 }
